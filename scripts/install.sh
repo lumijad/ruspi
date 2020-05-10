2af0c6bd0c50bafca0876e9adfe021a7faef0273
Rolf <rolf@scherer.ml>
2020-05-10 16:23:23 +0200

    tls example added

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,6 @@
+addgroup ruspi
+sudo useradd -M ruspi -g ruspi
+
 mkdir /opt/ruspi
 cp ./target/release/webserver /opt/ruspi/webserver
 cp -r ./config /opt/ruspi/config
@@ -5,4 +8,8 @@ cp -r ./static /opt/ruspi/static
 cp -r ./templates /opt/ruspi/templates
 cp -r ./tls /opt/ruspi/tls
 cp prod.env /opt/ruspi/.env
+chown -R ruspi /opt/ruspi
 
+cp ./scripts/ruspi.service /etc/systemd/system/ -v
+systemctl enable ruspi.service
+systemctl start ruspi.service
